07ac8552502921675edd317610d9f27ad4519890
Alvin MEIMOUN <alvin.meimoun@gmail.com>
2016-02-13 20:40:00 +0100

    fix(auth): Re-enabled social auth buttons

---
+++ Transcoder.WebApp.Web/Models/ExternalLoginListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transcoder.WebApp.Web.Models
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+}
