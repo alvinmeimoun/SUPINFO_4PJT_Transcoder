4180b1698a865d06320e3c80a5aeb9073ebc8779
Crysis108 <malfatti.antonin@gmail.com>
2016-05-15 16:10:13 +0200

    feat(service windows) : ajout des scripts batch pour install start stop

---
+++ Core.Transcoder.WindowsService/ProjectInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace Core.Transcoder.WindowsService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceInstaller serviceInstaller;
+        private ServiceProcessInstaller processInstaller;
+
+        public ProjectInstaller()
+        {
+            //InitializeComponent();
+            // Instantiate installers for process and services.
+            processInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            // The services run under the system account.
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            // The services are started manually.
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // ServiceName must equal those on ServiceBase derived classes.
+            serviceInstaller.ServiceName = "TranscoderWindowsService";
+
+            // Add installers to collection. Order is not important.
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
